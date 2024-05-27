c3ddbdb1209a09140d68a55a8e00529be72d877c
Fod9 <dernoncourt.tom@gmail.com>
2024-05-27 12:12:49 +0200

    size fixed

--- src/game/base.py
+++ src/game/base.py
@@ -36,16 +36,20 @@ class Base:
             self.h_percent = 1
             self.v_percent = 1
         elif self.age == 2:
-            pass
+            self.h_percent = 0.25
+            self.v_percent = 0.5
         elif self.age == 3:
-            self.heigth_offset = 0.8
+            self.h_percent = 1
+            self.v_percent = 1
+        elif self.age == 4:
+            self.h_percent = 0.3
+            self.v_percent = 0.55
 
     def take_damage(self, damage):
         self.HP -= damage
         self.last_damage = pygame.time.get_ticks()
         if self.HP <= 0:
             self.destroy()
-
 
     def destroy(self):
         self.HP = 0
@@ -73,7 +77,6 @@ class Base:
                 self.HP = self.max_health
             self.last_repair = current_time
 
-
     def draw(self, screen):
         # Define the percentage of the screen the base will take
         self.define_render_params()
@@ -85,15 +88,13 @@ class Base:
         # Define positions for the base
         if self.owner == "R":
             castle_x = screen_width - screen_width * self.h_percent
-            castle_y = (screen_height - screen_height * self.h_percent) * self.heigth_offset
+            castle_y = (screen_height - screen_height * self.v_percent) * self.heigth_offset
         else:
             castle_x = 0
-            castle_y = (screen_height - screen_height * self.h_percent) * self.heigth_offset
-
+            castle_y = (screen_height - screen_height * self.v_percent) * self.heigth_offset
 
         castle_width = screen_width * self.h_percent
-        castle_height = screen_height * self.h_percent
-
+        castle_height = screen_height * self.v_percent
 
         # Resize the base
         scaled_base_image = pygame.transform.scale(self.base_image, (int(castle_width), int(castle_height)))
@@ -113,12 +114,11 @@ class Base:
 
         health_bar_y = self.position[1] + castle_height / 4
 
-        #Show HP / Max HP
+        # Show HP / Max HP
         font = pygame.font.Font(None, 30)
         text = font.render(f"{self.HP}/{self.max_health}", True, (0, 0, 0))
         text_rect = text.get_rect(center=(health_bar_x + health_bar_width / 2, health_bar_y - 20))
         screen.blit(text, text_rect)
-
 
         # Draw the health bar background
         pygame.draw.rect(screen, (255, 0, 0), (health_bar_x, health_bar_y, health_bar_width, health_bar_height))
